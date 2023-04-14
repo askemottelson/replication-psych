25b12e283297d82afd07ef0857452312019a7289
Aske Mottelson <aske@mottelson.dk>
2023-04-14 12:42:59 +0200

    update

--- getdata.sh
+++ getdata.sh
@@ -1,11 +1,8 @@
-!wget https://www.dropbox.com/s/ppyp3izcb1us8hb/training_sample.csv?dl=1
-!mv training_sample.csv?dl=1 Data/training_sample.csv
-
-!wget https://www.dropbox.com/s/c4n74myrw5ryd02/dataset_test.csv?dl=1
-!mv dataset_test.csv?dl=1 Data/dataset_test.csv
-
-!wget https://www.dropbox.com/s/rhy90y1t75cst2c/full_training_data.csv?dl=1
-!mv full_training_data.csv?dl=1 Data/full_training_data.csv
-
-!wget https://www.dropbox.com/s/ppyp3izcb1us8hb/training_sample.csv?dl=1
-!mv training_sample.csv?dl=1 Data/training_sample.csv
+wget https://www.dropbox.com/s/ppyp3izcb1us8hb/training_sample.csv?dl=1
+mv training_sample.csv?dl=1 Data/training_sample.csv
+wget https://www.dropbox.com/s/c4n74myrw5ryd02/dataset_test.csv?dl=1
+mv dataset_test.csv?dl=1 Data/dataset_test.csv
+wget https://www.dropbox.com/s/rhy90y1t75cst2c/full_training_data.csv?dl=1
+mv full_training_data.csv?dl=1 Data/full_training_data.csv
+wget https://www.dropbox.com/s/ppyp3izcb1us8hb/training_sample.csv?dl=1
+mv training_sample.csv?dl=1 Data/training_sample.csv
